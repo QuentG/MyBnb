ea2255408ccea4b9dc8ce1978f4d73dd10cb86e6
Quentin Gans <quentin.gans@outlook.fr>
2019-04-09 00:04:21 +0200

    add entity Annonce + DataFixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Annonce;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+    	// Utilisation de la librairie Faker pour faire des fausses données "réaliste"
+    	$faker = Factory::create('fr_FR'); // Langue FR
+
+    	for ($i = 1; $i <= 30; $i++)
+    	{
+    		$annonce = new Annonce();
+
+    		$title = $faker->sentence();
+    		$coverImage = $faker->imageUrl(1000, 350);
+			$intro = $faker->paragraph(2);
+			$content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+			$annonce->setTitle($title)
+				->setCoverImage($coverImage)
+				->setIntroduction($intro)
+				->setContent($content)
+				->setPrice(mt_rand(50, 300))
+				->setRooms(2, 5)
+				->setSlug("slug-annonce-n°$i");
+
+			$manager->persist($annonce);
+		}
+
+        $manager->flush();
+    }
+}
---
+++ src/Repository/AnnonceRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Annonce;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Annonce|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Annonce|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Annonce[]    findAll()
+ * @method Annonce[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AnnonceRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Annonce::class);
+    }
+
+    // /**
+    //  * @return Annonce[] Returns an array of Annonce objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Annonce
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
