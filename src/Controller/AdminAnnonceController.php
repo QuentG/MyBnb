05629ec804b146953f285dced75cf68c192baab8
Quentin Gans <quentin.gans@outlook.fr>
2019-04-26 13:33:31 +0200

    Update design admin navbar + ACL admin

--- src/Controller/AdminAnnonceController.php
+++ src/Controller/AdminAnnonceController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\AnnonceRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminAnnonceController extends AbstractController
 {
@@ -20,7 +21,7 @@ class AdminAnnonceController extends AbstractController
 	private $manager;
 
 	/**
-	 * AnnonceController constructor.
+	 * AdminAnnonceController constructor.
 	 *
 	 * @param AnnonceRepository $repository
 	 * @param ObjectManager $manager
@@ -31,7 +32,12 @@ class AdminAnnonceController extends AbstractController
 		$this->manager = $manager;
 	}
 
-    public function index()
+	/**
+	 * Affiche toutes les annonces !
+	 *
+	 * @return Response
+	 */
+	public function index()
     {
     	$annonces = $this->repository->findAll();
         return $this->render('admin/annonce/index.html.twig', [
